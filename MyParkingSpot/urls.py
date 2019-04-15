b450742821a73d412b248e8c988c832e7b23ef52
swapnildabade24 <swapnildabade24@gmail.com>
2019-04-15 16:14:55 +0530

    Created model for Parking slot status and required minor changes

--- MyParkingSpot/urls.py
+++ MyParkingSpot/urls.py
@@ -1,4 +1,4 @@
-"""MyParkingSpot URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-]
+    path('home/', include('home.urls')),
+    path('', RedirectView.as_view(url='/home/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
