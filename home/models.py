a8b17a182c9924728d5c8b945bf61e3045e3f1af
swapnildabade24 <swapnildabade24@gmail.com>
2019-04-30 12:37:01 +0530

    Total slots and available slots count added

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class Slot_status(models.Model):
 
 	slot_id = models.CharField(max_length=5, help_text="Enter the Parking slot id (eg. s_01, s_02, etc.)")
 
-	status = models.BinaryField(max_length=1, help_text="1 for slot available and 0 for not availabel or blocked or reserved")
+	status = models.CharField(max_length=1, help_text="Y for slot available and N for not availabel or blocked or reserved")
 
 	def get_absolute_url(self):
 		# Returns the url to access a detail record for this slot
--- home/views.py
+++ home/views.py
@@ -4,8 +4,24 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 
+from home.models import Slot_status
+
 def index(request):
-    return HttpResponse("This is Index Page.")
+
+	#Genereate counts of the Parking Slots
+	slot_count = Slot_status.objects.all().count()
+
+	#Generate counts of available Parking slots
+	slot_available = Slot_status.objects.filter(status__exact='1').count()
+
+	context = {
+		'slot_count' : slot_count,
+		'slot_available' : slot_available,
+	}
+	# Render the HTML template index.html with the data in the context
+	return render(request, 'index.html', context=context)
+    #This is like hello world
+    #return HttpResponse("This is Index Page.")
 
 def login(request):
     return HttpResponse("This is Login Page.")
